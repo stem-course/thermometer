1d6fa742cbaa4d84ee4588dfcd01701e110530d8
areejasad98 <54935960+areejasad98@users.noreply.github.com>
2019-09-21 12:44:46 +0500

    Updated by Areej
    code changed

--- thermometer.ino
+++ thermometer.ino
@@ -1,54 +1,50 @@
+
 /*
-Code by:
-M.Hashir
+Code by: www.munphurid.com
+M.Hashir and Areej
 
 This is a code that display temperature and humidity measured by sensor
 
 Hardware:
--Arduino
--DHT11 sensor (temperature and humidity sensor)
+- Arduino
+- 3 wires
+- Munphurid dht11 module (Can be bought from www.munphurid.com)
 
 Connections:
 When perforated side (side with holes) of DHT sensor is towards you, the left most leg is pin 1 and right most leg is pin 4
-Connect pin 1 of DHT11 to 5V pin of Arduino
-Connect pin 2 of DHT11 to pin 2 of Arduino
-Connect pin 4 of DHT11 to GND pin of Arduino
+Connect pin 1 of dht11 to pin 5V of Arduino
+Connect pin 2 of dht11 to pin 2 of Arduino
+Connect pin 4 of dht11 to pin GND of Arduino
+
 */
-
-#include <DHT.h>      //Install this library using library manager
-
-int DHTPIN = 2;     //DHT is connected to this pin
-#define DHTTYPE DHT11   // We are using this model of DHT..... DHT11... Do not edit this
-
-DHT dht(DHTPIN, DHTTYPE); //Do not edit this line
-
-
-float hum;  //Do not edit
-float temp; //Do not edit
-
-void setup() 
-{
-  Serial.begin(9600);
-  Serial.println("DHT11 sensor testing");   //Print this on Serial monitor when program starts
-  dht.begin();                //Do not edit
-}
-
-void loop() 
-{ 
-    float converted = 0.00; //Do not edit
-        
-    hum = dht.readHumidity();         //Read Humidity value from sensor
-    temp= dht.readTemperature();      //Read temperature value from sensor
-
-    Serial.print("Celsius = ");     //Print this on Serial monitor
-    Serial.print(temp);             //Print temperature on Serial monitor
+#include <dht11.h>                                               //Install this library using library manager
+#define dht_apin 2                                               //We are using this model of DHT and is connected to pin 2..... DHT11... Do not edit this
+ 
+dht11 DHT;
+ 
+void setup(){
+ 
+  Serial.begin(9600);                                            //donot edit this line
+  delay(1000);                                                   //wait for 1 second
+  Serial.println("DHT11 Humidity & temperature Sensor\n\n");     //Print this on Serial monitor when program starts
+  delay(1000);                                                   //wait for 1 second
+ 
+}           
+ 
+void loop(){
+  
+ 
+    DHT.read(dht_apin);                            //Read Humidity and Temperature value from sensor
     
-    //Print degree symbol
-    Serial.write(176);                      //Do not edit
-    Serial.println("C");                    //Do not edit
+    Serial.print("Current humidity = ");           //Print this on Serial monitor
+    Serial.print(DHT.humidity);                    //Print the value of Humidity on serial monitor
+    Serial.print("%  ");                           //donot edit this line
+    Serial.print("temperature = ");                //Print this on Serial monitor
+    Serial.print(DHT.temperature);                 //Print the value of Temperature on serial monitor
+    Serial.println("C  ");                         //donot edit this line
+    
+    delay(5000);                                   //Wait for 5 seconds  
+ 
 
-    Serial.print("Humidity =");       //Print this on Serial monitor
-    Serial.println(hum);              //Print value of humidity on Serial monitor
-
-    delay(2000);          //Wait for 2 seconds
+ 
 }
